d16fcf3172ecbc22ab2d5de520a02fb4db1f4470
Shell.Xu <shell909090@gmail.com>
2014-12-24 17:42:40 +0800

    * 修改syscall保证做系统调用。

--- s_syscall.c
+++ s_syscall.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+#include <sys/syscall.h>
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +18,8 @@ int main(int argc, char *argv[])
 	n = atoi(argv[1]);
 
 	for (i = 0; i < n; i++) {
-		pid = getpid();
+		pid = syscall(SYS_getpid);
+		/* pid = getpid(); */
 	}
 	
 	return 0;
